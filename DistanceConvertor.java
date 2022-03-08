a3fc7419f09e586e84bf9ad49a7ca0d39176d35a
gharshitha80 <gundalaharshitha@gmail.com>
2022-03-08 09:38:57 +0100

    initial

---
+++ DistanceConvertor.java
@@ -0,0 +1,30 @@
+public class DistanceConvertor{
+    public static void main(String [] args){
+    }
+    
+    public int kms_to_metres(int kms){
+    int meters= kms*1000;
+    return meters;
+    }
+    public int kms_to_cm(int kms){
+    int cm= kms*100000;
+    return cm;
+    }
+    public int foot_to_inch(int foot){
+    int inch= foot*12;
+    return inch;
+    }
+    public int meters_to_cm(int meters){
+    int cm=meters*100;
+    return cm;
+    }
+    public int meters_to_mm(int meters){
+    int mm=meters*1000;
+    return mm;
+    }
+    public int cm_to_mm(int cm){
+    int mm=cm*10;
+    return mm;
+    }
+    }
+    
